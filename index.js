20a4b28380c99afb887f05ad77a70aea86155df2
MJ Cariso <111568194+emjay03@users.noreply.github.com>
2023-05-24 20:06:37 +0800

    server is running

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express()
 app.listen(3001, () => 
 console.log('Server running on port 3001'))
 
-app.get( '/', (req, res) =>res.json('Hello World!'))
+app.get( '/', (req, res) =>res.json('The server is running perfectly !'))
